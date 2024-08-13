bd21a874ac6e9965d69302005ca107a44f106211
Iryna S. <78602594+au-rinko@users.noreply.github.com>
2024-08-13 13:54:10 +0300

    Done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,32 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  const res = [];
+  const openBrackets = bracketsConfig.map(pair => pair[0]);
+  const closeBrackets = bracketsConfig.map(pair => pair[1]);
+
+  if(str % 2 === 1){
+    return false;
+  }
+
+  for(let i = 0; i < str.length; i++) {
+    if(openBrackets.includes(str[i])){
+      if(res[res.length - 1] === str[i] && closeBrackets.includes(str[i])){
+        res.pop();
+      }else{
+        res.push(str[i]);
+      }
+    }else if(closeBrackets.includes(str[i])){
+      if(res.length > 0){
+        if(res[res.length - 1] == openBrackets[closeBrackets.indexOf(str[i])]){
+          res.pop();
+        }else{
+          return false;
+        }
+      }else{
+        return false;
+      }
+    }
+  }
+
+  return res.length === 0;
+
 }
